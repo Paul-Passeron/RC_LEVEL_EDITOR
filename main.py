e2a1aa49936209173ecab61fb826758ca4500d11
Paul-Passeron <paul.passeron2@gmail.com>
2023-04-04 17:48:57 +0200

    Included possibility to switch editing mode

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from math import floor
 import sys
 from random import random
 from enum import IntEnum, auto
+
+
 """Structure of .rcmap files:
 
 #HEADBEGIN
@@ -33,8 +35,8 @@ SCREEN_WIDTH      = 600
 SCREEN_HEIGHT     = 600
 
 GRID_COLOR        = (255/2, 255/2, 255/2)
-
 LINE_RED          = (255*0.7, 0, 0)
+LINE_GREY         = (255*0.75, 255*0.75, 255*0.75)
 
 SCALING_FACTOR    = 4
 
@@ -69,6 +71,8 @@ MAP[ObjectType.Player]        = []
 MAP[ObjectType.Collectible]   = []
 
 
+CURRENT_MODE = ObjectType.Wall
+
 def parse_list(lis, n=3):
     buffer = ""
     for i, w in enumerate(lis):
@@ -82,7 +86,7 @@ def parse_list(lis, n=3):
     return buffer
 
 
-def create_rcmap_from_dic(filename, dic):
+def create_rcmap_from_dic(filename, dict):
     with open(filename, 'w') as f:
         sys.stdout = f
         walls = dict[ObjectType.Wall]
@@ -153,6 +157,8 @@ done = False
 
 while not done: #Program Loop
 
+    #Clearing the screen
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
     #Drawing grid lines and dots
     draw_grid()
 
@@ -166,29 +172,45 @@ while not done: #Program Loop
 
     if len(CURRENT_LINE) >= 4:
         CURRENT_LINE = []
-    
-    if IS_DRAGGING and len(CURRENT_LINE) == 0:
-        CURRENT_LINE = [i, j]
-
-    if not IS_DRAGGING and WAS_DRAGGING and len(CURRENT_LINE) == 2:
-        CURRENT_LINE.append(i)
-        CURRENT_LINE.append(j)
-        CURRENT_LINE.append(1)
-        CURRENT_LINE.append(CURRENT_TEXTURE)
-        LINES.append(CURRENT_LINE)
+    if CURRENT_MODE == ObjectType.Wall:
+        if IS_DRAGGING and len(CURRENT_LINE) == 0:
+            CURRENT_LINE = [i, j]
+        if IS_DRAGGING: #Showing preview line:
+            draw_line(CURRENT_LINE[0], CURRENT_LINE[1], i, j, LINE_GREY)
+        if not IS_DRAGGING and WAS_DRAGGING and len(CURRENT_LINE) == 2:
+            CURRENT_LINE.append(i)
+            CURRENT_LINE.append(j)
+            CURRENT_LINE.append(1)
+            CURRENT_LINE.append(CURRENT_TEXTURE)
+            MAP[ObjectType.Wall].append(CURRENT_LINE)
 
     #Drawing lines
-    for (i1, j1, i2, j2, _, _) in LINES:
+    for (i1, j1, i2, j2, _, _) in MAP[ObjectType.Wall]:
         draw_line(i1, j1, i2, j2, LINE_RED)
 
     #Handling quitting the window and outputting the .rcmap file when ENTER is rekeased.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            done = True
         if event.type == pygame.KEYDOWN and event.key == pygame.K_KP_ENTER:
-            create_rcmap_from_lists("test.rcmap", "", LINES, [], (0, 0))
+            create_rcmap_from_dic("test.rcmap", MAP)
             print("Map Saved!")
 
+        #Handling different mode 
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+            CURRENT_MODE = ObjectType.Wall
+            print("Wall mode")
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_i:
+            CURRENT_MODE = ObjectType.Item
+            print("Item mode")
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+            CURRENT_MODE = ObjectType.Collectible
+            print("Collectibe mode")
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+            CURRENT_MODE = ObjectType.Player
+            print("Player mode")
     #Updating the screen
     pygame.display.flip()
 
